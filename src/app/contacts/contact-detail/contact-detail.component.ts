e516734040c4ed082cde45f6aa86b420de63f95b
Hannah White <lea16001@byui.edu>
2020-09-25 20:21:54 -0600

    contact detail & contact list implemented

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailComponent implements OnInit {
 
+  contact: Contact = 
+      new Contact('1', 
+      'R. Kent Jackson', 
+      'jacksonk@byui.edu', 
+      '208-496-3771', 
+      'https://web.byui.edu/Directory/Employee/jacksonk.jpg',
+      null);
+      
   constructor() { }
 
   ngOnInit(): void {
